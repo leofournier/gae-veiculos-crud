fa3d4b48edc9d5f101751ffb5fd8a900db954003
Leonardo Fournier <leonardo@lfournier.com.br>
2017-04-26 21:31:50 -0300

    autenticação para a página inicial

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 Sistema de Cadastro de Veículos 
 
-- Banco de dados: (NOSQL) Datastore
-- Linguagem: Python
+- Banco de dados: Google Datastore (NOSQL)
+- Linguagem: Python 2.7
 - Ambiente de Deploy: Google Cloud Platform - Appengine
-- Outros: Bootstrap e Jinja
 
 Para acessar o sistema basta ir em [veiculos-crud.appspot.com] (https://veiculos-crud.appspot.com/) e acessar com uma conta Google (qualquer uma @gmail.com ou seu dominio sob o Google Apps for Business).
--- views.py
+++ views.py
@@ -17,10 +17,15 @@ jinja_environment.globals['year'] = datetime.now().year
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        veic = Veiculo() 
-        template_values = {'veiculo' : veic.listar_veiculo()}
-        template = jinja_environment.get_template('template/index.html')
-        self.response.out.write(template.render(template_values))
+        user = users.get_current_user()
+        if user:
+            veic = Veiculo() 
+            template_values = {'veiculo' : veic.listar_veiculo()}
+            template = jinja_environment.get_template('template/index.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
     def post(self):
         user = users.get_current_user()
         if user:
@@ -57,18 +62,15 @@ class EditHandler(webapp2.RequestHandler):
     def get (self):
         user = users.get_current_user()
         if user:
-            #get ID of entity Key
             veic_k = db.Key.from_path('CadastroModel','LFournier','VeiculoModel',long(self.request.get('id')))
-            #get entity from key instance
             veic = db.get(veic_k)
-            
+
             template_values = {'veiculo' : veic}
             template = jinja_environment.get_template('template/edit.html')
             self.response.out.write(template.render(template_values))
         else:
             self.redirect(users.create_login_url(self.request.uri))
     def post(self):
-        #get all input values
         input_id = self.request.get('id')
         input_modelo = self.request.get('modelo').strip()
         input_fabricante = self.request.get('fabricante').strip()
